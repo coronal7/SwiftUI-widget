c43a79119483ae1e379adfb48be2d051ea5733f9
Gunjan Tripathi <gunjantripathi7@gmail.com>
2020-10-15 13:02:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,160 @@
+import React from "react"
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import SharedGroupPreferences from 'react-native-shared-group-preferences'
+const appGroupIdentifier = "group.com.groww"
+
+export default class app extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: undefined
+    }
+    if (Platform.OS == 'android') {
+      this.dealWithPermissions()
+    } else {
+      this.saveUserDataToSharedStorage(userData)
+    }
+  }
+  async dealWithPermissions() {
+    try {
+      const grantedStatus = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+      ])
+      const writeGranted = grantedStatus["android.permission.WRITE_EXTERNAL_STORAGE"] === PermissionsAndroid.RESULTS.GRANTED
+      const readGranted = grantedStatus["android.permission.READ_EXTERNAL_STORAGE"] === PermissionsAndroid.RESULTS.GRANTED
+      if (writeGranted && readGranted) {
+        this.saveUserDataToSharedStorage(userData)
+      } else {
+      }
+    } catch (err) {
+      console.warn(err)
+    }
+  }
+  async saveUserDataToSharedStorage(data) {
+    try {
+      const appGroupIdentifier = 'group.com.groww';
+
+		let myAppData = {
+      c_name: "Regina Phalange",
+      c_age: 28,
+      c_email: "rphalange@gmail.com"
+		};
+      await SharedGroupPreferences.setItem("myAppData", myAppData, appGroupIdentifier)
+      this.loadUsernameFromSharedStorage()
+    } catch(errorCode) {
+      console.log(errorCode)
+    }
+  }
+  async loadUsernameFromSharedStorage() {
+    try {
+      const loadedData = await SharedGroupPreferences.getItem("myappData", appGroupIdentifier)
+      this.setState({username:loadedData.name})
+    } catch(errorCode) {
+      console.log(errorCode)
+    }
+  }
+  render() {
+    return (
+      <>
+    
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                screen and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
